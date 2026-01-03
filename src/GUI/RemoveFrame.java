6ea513207cfc76e0e3315405647f8ac819985595
S-Efekan <sahin.efekann@gmail.com>
2026-01-03 22:11:26 +0300

    New Frame

--- src/GUI/RemoveFrame.java
+++ src/GUI/RemoveFrame.java
@@ -5,22 +5,52 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import java.awt.Window.Type;
 
 public class RemoveFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
 
 
 	
 	public RemoveFrame() {
+		setTitle("REMOVE FRAME");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 270);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(185, 6, 127, 27);
+		contentPane.add(comboBox);
+		
+		JLabel lblNewLabel_1 = new JLabel("ID:");
+		lblNewLabel_1.setBounds(175, 50, 18, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(195, 45, 130, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("STATUS");
+		lblNewLabel_2.setBounds(219, 210, 51, 26);
+		contentPane.add(lblNewLabel_2);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setLineWrap(true);
+		textArea.setBounds(163, 150, 166, 72);
+		contentPane.add(textArea);
 
 	}
-
 }
